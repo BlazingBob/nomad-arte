1e626fe239c3ff7e997550363fb69e86261c2b57
Jip Teeuwen <jipteeuwen@hotmail.com>
2023-05-30 17:40:07 +0200

    add new and create page incl. styling

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  validates :name, :description, :price_per_day, presence: true
+  validates :description, length: { minimum: 20 }
+  validates :name, uniqueness: { scope: :user, message: 'You already have this item listed' }
+  validates :price_per_day, numericality: { less_than_or_equal_to: 10_000, greater_than: 1, only_integer: true }
 end
