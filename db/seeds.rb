63a5356c9d4272bd2e80391a9bef6eaf2686c648
katia thume <katiathume@gmail.com>
2023-05-30 23:30:18 +0200

    Add image url field to Items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Item.destroy_all
+User.destroy_all
+lunna = User.create!(email: "lunna@hotmail.com", password: "123456!", firstname: "Lunna", lastname: "Thume", username: "Lunna's")
+Item.create!(name: "Lunna", description: "Sculpture" , price_per_day: "50$", category_name: "Modernismo", user: lunna, image_url: "http://sothebys-brightspot.s3.amazonaws.com/media-desk/95/9b/53fc72194c30ac2cd94b3dc96163/pf2305-9yzk5-cs-11-cropped.jpg")
+Item.create!(name: "Lua", description: "Mirror" , price_per_day: "20$", category_name: "Baroque", user: lunna, image_url: "https://img5.su-cdn.com/cdn-cgi/image/width=600,height=600,format=webp/mall/file/2023/05/15/77f1a7ecb97a0097b363f5f152b29495.jpg")
