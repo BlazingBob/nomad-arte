57f49fa379d965d48fbda581f5ca8f5afa4ca5bb
katia thume <katiathume@gmail.com>
2023-05-31 16:08:39 +0200

    Update form with new atribute

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,7 @@ class ItemsController < ApplicationController
     @item = Item.new(item_params)
     @item.user = current_user
     if @item.save
-      redirect_to dashboard_path
+      redirect_to item_path(@item)
     else
       render :new, status: :unprocessable_entity
     end
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :price_per_day, :category_name, :photo)
+    params.require(:item).permit(:name, :description, :price_per_day, :category_name, :image)
   end
 end
