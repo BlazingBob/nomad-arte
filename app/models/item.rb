056efa2cbe22b04c5dfb542e52dfede21ec6cfbc
Jip Teeuwen <jipteeuwen@hotmail.com>
2023-06-01 16:16:10 +0200

    add form styles and navbar button

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,8 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   validates :name, :description, :price_per_day, presence: true
-  validates :description, length: { minimum: 1 }
+  validates :description, length: { minimum: 5 }
   validates :name, uniqueness: { scope: :user, message: 'You already have this item listed' }
   validates :price_per_day, numericality: { less_than_or_equal_to: 10_000, greater_than: 1, only_integer: true }
+  validates :image, presence: { message: 'must be provided' }
 end
