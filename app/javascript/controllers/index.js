d0be39b088cd73b4efcb8094f1e51939fb5c8b7c
Jip Teeuwen <jipteeuwen@hotmail.com>
2023-07-13 11:36:29 +0200

    Add stripe JS controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import StripeController from "./stripe_controller"
+application.register("stripe", StripeController)
